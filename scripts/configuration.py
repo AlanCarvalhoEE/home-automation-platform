346415cbe48fdc3b896d31e4d2f721b09156cb6e
AlanCarvalhoEE <alan.amaral.carvalho@gmail.com>
2023-09-22 17:22:32 -0300

    General comments and indentation fixes

--- scripts/configuration.py
+++ scripts/configuration.py
@@ -1,9 +1,8 @@
-# File to configure GPIO, network and others
+import RPi.GPIO as GPIO     # Library for GPIO manipulation
 
-import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)      # Use pin numbering based on the Broadcom chip
 
-GPIO.setmode(GPIO.BCM)
-relays=[4, 17, 27, 22, 5, 6, 26, 23, 24, 36]
+relays=[4, 17, 27, 22, 5, 6, 26, 23, 24, 36]    # Map the relay number (1 to 10) to the pin numbering
 
-for i in range(9):
+for i in range(9):      # Configure all the relay pins as outputs
     GPIO.setup(relays[i], GPIO.OUT)
--- scripts/devices.py
+++ scripts/devices.py
@@ -1,9 +1,12 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO     # Library for GPIO manipulation
 
+# Function to control relays
+# parameter 1 = relay number (1 to 10)
+# parameter 2 = relay state (0 = OFF, 1 = ON)
 def setRelay(number, state):
-        if state:
-                print("HIGH")
-                GPIO.output(number, GPIO.HIGH)
-        else:
-                print("LOW")
-                GPIO.output(number, GPIO.LOW)
+    if state:
+        print("HIGH")
+        GPIO.output(number, GPIO.HIGH)
+    else:
+        print("LOW")
+        GPIO.output(number, GPIO.LOW)
