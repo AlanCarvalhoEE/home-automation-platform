cd2cdcc2742f3d2bd8280fd2900b251816bb9f82
AlanCarvalhoEE <alan.amaral.carvalho@gmail.com>
2026-02-21 15:55:22 -0300

    Firmware OTA implemented on firmware.

--- firmware/on_off_module_x1/credentials_example.h
+++ firmware/on_off_module_x1/credentials_example.h
@@ -3,3 +3,6 @@ const char* ssid = "*******";             // Network SSID
 const char* password = "********";        // Network password
 const char* brokerIP = "***.***.**.***";  // Broker IP
 const uint16_t brokerPort = ****;         // Broker port
+
+#define FW_SERVER_IP "***.***.*.**"       // Firmware update URL
+#define FW_SERVER_PORT **                 // Firmware update port
